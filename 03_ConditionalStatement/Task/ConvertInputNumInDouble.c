75aeb9aff4f8a5aead0aaa316c963b75d32e95e4
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-07 21:21:47 +0530

    Convert User Input Number in double

---
+++ 03_ConditionalStatement/Task/ConvertInputNumInDouble.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int num1, ans;
+
+    printf("\nEnter the number : ");
+    scanf("%d", &num1);
+
+    num1 *= 2;
+    printf("\nThe double of the number is : %d", num1);
+}
