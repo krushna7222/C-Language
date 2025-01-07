2b0135c9442d038e8cfa31568e8a58fe5a7e1ba5
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-07 21:20:15 +0530

    If Statement in C Language

---
+++ 03_ConditionalStatement/01_if.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    int a = 10;
+
+    if (a > 5) // If ths condition is true then exicute this block
+    {
+        printf("\nThe Value Of s is :%d\n", a);
+    }
+}
