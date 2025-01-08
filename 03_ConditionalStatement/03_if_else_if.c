96277e075ecec1c2037dfb996a86ba55b0ba463b
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-08 21:47:03 +0530

    if else if loop in JavaScript

---
+++ 03_ConditionalStatement/03_if_else_if.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+void main()
+{
+    int marks;
+    printf("\nEnter your marks and Get Your Grade : ");
+    scanf("%d", &marks);
+
+    if (marks > 100)
+    {
+        printf("Invalid Marks Plz Enter Valid Mark !!! ");
+    }
+    else if (marks >= 90 && marks <= 100)
+    {
+        printf("Congratulations Your Gread is A+  !!!");
+    }
+    else if (marks >= 80 && marks < 90)
+    {
+        printf("Congratulations Your Gread is A !!!");
+    }
+    else if (marks >= 70 && marks < 80)
+    {
+        printf("Congratulations Your Gread is B+ !!!");
+    }
+    else if (marks >= 55 && marks < 70)
+    {
+        printf("Congratulations Your Gread is B !!!");
+    }
+    else if (marks >= 45 && marks < 55)
+    {
+        printf("Congratulations Your Gread is C !!!");
+    }
+    else if (marks >= 35 && marks < 45)
+    {
+        printf("Your Gread is D !!!");
+    }
+    else
+    {
+        printf("Sorry, your Result is failed. !!!");
+    }
+}
