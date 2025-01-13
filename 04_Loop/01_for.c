b79282b0ce76c351bbbec97edef0abbf975a1253
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-13 22:10:20 +0530

    for loop in C language

---
+++ 04_Loop/01_for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+
+    // First initilize condition (starting Point of loop)
+    // End Point of loop
+    // increment or dewcrement condition
+
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("Hello World !!!  %d \n", i);
+    }
+}
