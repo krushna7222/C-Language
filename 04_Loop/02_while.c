af8c0d3b78c1e888c4f8d794dd06dcbf40c64d9e
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-14 18:35:31 +0530

    while loop in C language

--- 04_Loop/02_while.c
+++ 04_Loop/02_while.c
@@ -3,7 +3,7 @@ void main()
 {
     int i = 1;
 
-    while (i <= 10)
+    while (i <= 10) // Use a while loop when the number of iterations is not known beforehand, and you want to check the condition before executing the loop.
     {
         printf("\nThe Value Of i Is : %d", i);
         i++;
