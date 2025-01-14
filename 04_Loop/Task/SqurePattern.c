faac4b2469f68c974ce636f9dd055ac9f0bfbad8
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-14 18:39:43 +0530

    print squre pattern using C language get row and column Number from user

---
+++ 04_Loop/Task/SqurePattern.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    int row, colm;
+    printf("Enter the number of rows: ");
+    scanf("%d", &row);
+    printf("Enter the number of columns: ");
+    scanf("%d", &colm);
+}
