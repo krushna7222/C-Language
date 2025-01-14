2dbbce4b5dd4c43dc7e221a1794fe2ea7d39b60d
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-14 19:03:04 +0530

    Squre Pattern printing using C row and column Number take from user

--- 04_Loop/Task/SqurePattern.c
+++ 04_Loop/Task/SqurePattern.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 void main()
 {
-    int row, colm;
+    int row, colm, i, j;
     printf("Enter the number of rows: ");
     scanf("%d", &row);
     printf("Enter the number of columns: ");
     scanf("%d", &colm);
+
+    for (i = 0; i <= row; i++)
+    {
+        printf("\n*");
+        for (j = 1; j <= colm; j++)
+        {
+            printf("\t*");
+        }
+    }
 }
