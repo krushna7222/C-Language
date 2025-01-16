3c5149eb3a5ebb1db5564ae0d8925146aeb761a1
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-01-16 22:33:43 +0530

    Array in C

---
+++ 05_Array/01_Array2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+
+    int marks[] = {85, 96, 45, 74, 58, 69, 85, 78};
+
+    for (int i = 0; i < marks[i]; i++)
+    {
+        printf("\nThe marks of student id : %d", marks[i]);
+    }
+}
